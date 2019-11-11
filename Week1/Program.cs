ac5b402bc76d57e35e5e2144c3f13504f96bebc4
Yornie <jinxie88@live.nl>
2019-11-11 15:27:48 +0100

    Assignment 0 + Start Assignment 1
    Enums, structs and classes

---
+++ Week1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    class Program
+    {
+        private string _question1 = "tik een getal: ";
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Start();
+        }
+
+        void Start()
+        {
+            PrintMonths();
+            Console.WriteLine(_question1);
+            int n = ReadInt(_question1);
+            Console.WriteLine("Je hebt {0} ingetikt.", n);
+
+            int age = ReadInt("Hoe oud ben je ? ", 0, 120);
+            Console.WriteLine("Je bent {0} jaar oud.", age);
+
+            string name = ReadString("Hoe heet je ?");
+            Console.WriteLine("Aangenaam kennis met je te maken, {0}.", name);
+            Console.ReadKey();
+        }
+
+        private int ReadInt(string question)
+        {
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+
+        private int ReadInt(string question, int min, int max)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(num >= min && num <= max)
+            {
+                return num;
+            } else
+            {
+                //opnieuw invoeren
+                return num;
+            }
+        }
+
+        private string ReadString(string question)
+        {
+            string n = Console.ReadLine();
+            return n;
+        }
+
+        private void PrintMonth(Months month)
+        {
+            Console.WriteLine(month.ToString());
+        }
+
+        private void PrintMonths()
+        {
+            foreach(Months month in Enum.GetValues(typeof(Months)))
+            {
+                int num = (int)month;
+                Console.WriteLine("{0}. {1}", num, month);
+            }
+        }
+
+        private Months AskMonth(string question)
+        {
+            Console.WriteLine("Voer maand nummer in: ");
+            int num = int.Parse(Console.ReadLine());
+            Months m = (Months)Enum.Parse(typeof(Months), num.ToString());
+            if (num > 0 && num < 13)
+            {
+                return m;
+            }
+            else
+            {
+                throw num;
+            }
+        }
+    }
+}
