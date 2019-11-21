b0b199e5c8989ad646508b57e45be83bb8e86654
Yornie <jinxie88@live.nl>
2019-11-21 14:34:50 +0100

    Start opdracht 1 Week 2
    - Bug in matrix that it doesnt show the first row correctly

--- Week2Opdracht1/Program.cs
+++ Week2Opdracht1/Program.cs
@@ -8,8 +8,51 @@ namespace Week2Opdracht1
 {
     class Program
     {
+        private int[,] _matrix;
+        private int _col;
+        private int _row;
+        private Random _rand = new Random();
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Start();
+        }
+
+        private void Start()
+        {
+            Console.WriteLine("Amount of Cols");
+            _col = int.Parse(Console.ReadLine());
+            Console.WriteLine("Amount of Rows");
+            _row = int.Parse(Console.ReadLine());
+            _matrix = new int[_col, _row];
+            InitMatrix(_matrix);
+            PrintMatrix(_matrix);
+            Console.ReadKey();
+        }
+
+        private void InitMatrix(int[,] matrix)
+        {
+            int value = 1;
+            for (int r = 1; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 1; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = r * c + 1;
+                }
+            }
+        }
+
+        private void PrintMatrix(int[,] matrix)
+        {
+            for(int r = 1; r < matrix.GetLength(0); r++)
+            {
+                for(int c = 1; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
