a17e854ff05a0244b3522d360e028be4bc5a0eab
Yornie <jinxie88@live.nl>
2019-11-27 21:28:43 +0100

    opdracht 1 c & d
    - Linear matrix
    - Matrix with cross drawn

--- Week2Opdracht1/Program.cs
+++ Week2Opdracht1/Program.cs
@@ -9,8 +9,6 @@ namespace Week2Opdracht1
     class Program
     {
         private int[,] _matrix;
-        private int _col;
-        private int _row;
         private Random _rand = new Random();
 
         static void Main(string[] args)
@@ -21,25 +19,32 @@ namespace Week2Opdracht1
 
         private void Start()
         {
-            Console.WriteLine("Amount of Cols");
-            _col = int.Parse(Console.ReadLine());
-            Console.WriteLine("Amount of Rows");
-            _row = int.Parse(Console.ReadLine());
-            _matrix = new int[_row, _col];
-            InitMatrix(_matrix);
-            PrintMatrix(_matrix);
+            Console.WriteLine("Input size");
+            int size = int.Parse(Console.ReadLine());
+            _matrix = new int[size, size];
+            InitMatrixLinear(_matrix);
+            PrintMatrixWithCross(_matrix);
             Console.ReadKey();
         }
 
         private void InitMatrix(int[,] matrix)
         {
             int num = 1;
-            for (int r = 0; r < matrix.GetLength(0); r++)
+            for(int r = 0; r < matrix.GetLength(0); r++)
             {
                 for (int c = 0; c < matrix.GetLength(1); c++)
                 {
                     matrix[r, c] = num++;
                 }
+            }
+        }
+
+        private void InitMatrixLinear(int[,] matrix)
+        {
+            int s = (matrix.GetLength(0) * matrix.GetLength(0));
+            for(int i = 1; i <= s; i++)
+            {
+                matrix[(i - 1) / matrix.GetLength(0), (i - 1) % matrix.GetLength(0)] = i;
             }
         }
 
@@ -50,6 +55,32 @@ namespace Week2Opdracht1
                 for(int c = 0; c < matrix.GetLength(1); c++)
                 {
                     Console.Write(matrix[r, c] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintMatrixWithCross(int[,] matrix)
+        {
+            int l = matrix.GetLength(0);
+
+            for (int r = 0; r < l; r++)
+            {
+                for (int c = 0; c < l; c++)
+                {
+                    Console.ResetColor();
+                    if (r == c)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+
+                    if (c == (matrix.GetLength(0) - r - 1))
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    }
+
+                    Console.Write(matrix[r, c] + "\t");
+                    
                 }
                 Console.WriteLine();
             }
